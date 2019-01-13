792adc44a7f5b002933b5a27e2b3825efcf5037c
BristolTransformed <46425217+BristolTransformed@users.noreply.github.com>
2019-01-13 19:45:31 +0000

    Update main.js

--- bristoltransformed/assets/js/main.js
+++ bristoltransformed/assets/js/main.js
@@ -5,32 +5,28 @@
 */
 
 (function($) {
-	// Get the modal
-	var modal = btn.getAttribute('data-modal');
-	document.getElementById('myModal');
+	
+ var modalBtns = [...document.querySelectorAll(".button")];
+modalBtns.forEach(function(btn){
+  btn.onclick = function() {
+    var modal = btn.getAttribute('data-modal');
+    document.getElementById(modal).style.display = "block";
+  }
+});
 
-// Get the button that opens the modal
-	var btn = document.getElementById("myBtn");
+var closeBtns = [...document.querySelectorAll(".close")];
+closeBtns.forEach(function(btn){
+  btn.onclick = function() {
+    var modal = btn.closest('.modal');
+    modal.style.display = "none";
+  }
+});
 
-// Get the <span> element that closes the modal
-	var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal 
-	btn.onclick = function() {
-	  modal.style.display = "block";
-	}
-
-// When the user clicks on <span> (x), close the modal
-	span.onclick = function() {
-	  modal.style.display = "none";
-	}
-
-// When the user clicks anywhere outside of the modal, close it
-	window.onclick = function(event) {
-	  if (event.target == modal) {
-	    modal.style.display = "none";
-	  }
-	}
+window.onclick = function(event) {
+  if (event.target.className === "modal") {
+    event.target.style.display = "none";
+  }
+}
 
 	var	$window = $(window),
 		$body = $('body');
