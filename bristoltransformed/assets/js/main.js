b07f52518441d36528fdb8bf3f4eb569245bdd31
BristolTransformed <46425217+BristolTransformed@users.noreply.github.com>
2019-01-13 19:38:56 +0000

    Update main.js

--- bristoltransformed/assets/js/main.js
+++ bristoltransformed/assets/js/main.js
@@ -6,7 +6,8 @@
 
 (function($) {
 	// Get the modal
-	var modal = document.getElementById('myModal');
+	var modal = btn.getAttribute('data-modal');
+	document.getElementById('myModal');
 
 // Get the button that opens the modal
 	var btn = document.getElementById("myBtn");
