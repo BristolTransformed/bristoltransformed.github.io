77dce7a406de7496a28c41bc216b58bfe2426cda
BristolTransformed <46425217+BristolTransformed@users.noreply.github.com>
2019-01-14 16:36:53 +0000

    Update main.js

--- bristoltransformed/assets/js/main.js
+++ bristoltransformed/assets/js/main.js
@@ -6,6 +6,7 @@
 
 (function($) {
 
+
 	var	$window = $(window),
 		$body = $('body');
 
@@ -63,5 +64,26 @@
 					target: $body,
 					visibleClass: 'navPanel-visible'
 				});
+	var modalBtns = [...document.querySelectorAll(".button")];
+	modalBtns.forEach(function(btn){
+  		btn.onclick = function() {
+    			var modal = btn.getAttribute('data-modal');
+   	 document.getElementById(modal).style.display = "block";
+  		}
+	});
+
+	var closeBtns = [...document.querySelectorAll(".close")];
+	closeBtns.forEach(function(btn){
+  		btn.onclick = function() {
+    			var modal = btn.closest('.modal');
+    			modal.style.display = "none";
+  		}
+	});
+
+	window.onclick = function(event) {
+  		if (event.target.className === "modal") {
+    			event.target.style.display = "none";
+  		}
+	}
 
 })(jQuery);
