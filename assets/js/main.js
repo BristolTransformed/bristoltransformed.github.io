383851f3705fd08f5edc865ce665ce44247516c2
BristolTransformed <46425217+BristolTransformed@users.noreply.github.com>
2019-02-06 21:27:38 +0000

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,91 @@
+/*
+	ZeroFour by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function($) {
+
+
+	var	$window = $(window),
+		$body = $('body');
+
+	// Breakpoints.
+		breakpoints({
+			xlarge:  [ '1281px',  '1680px' ],
+			large:   [ '981px',   '1280px' ],
+			medium:  [ '737px',   '980px'  ],
+			small:   [ null,      '736px'  ]
+		});
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Dropdowns.
+		$('#nav > ul').dropotron({
+			offsetY: -22,
+			mode: 'fade',
+			noOpenerFade: true,
+			speed: 300,
+			detach: false
+		});
+
+	// Nav.
+
+		// Title Bar.
+			$(
+				'<div id="titleBar">' +
+					'<a href="#navPanel" class="toggle"></a>' +
+					'<span class="title">' + $('#logo').html() + '</span>' +
+				'</div>'
+			)
+				.appendTo($body);
+
+		// Panel.
+			$(
+				'<div id="navPanel">' +
+					'<nav>' +
+						$('#nav').navList() +
+					'</nav>' +
+				'</div>'
+			)
+				.appendTo($body)
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					hideOnSwipe: true,
+					resetScroll: true,
+					resetForms: true,
+					side: 'left',
+					target: $body,
+					visibleClass: 'navPanel-visible'
+				});
+
+
+	
+	var modalBtns = [...document.querySelectorAll(".buttonsmall")];
+	modalBtns.forEach(function(btn){
+  		btn.onclick = function() {
+ 	   		var modal = btn.getAttribute('data-modal');
+	document.getElementById(modal).style.display = "block";
+ 	 	}
+	});
+
+	var closeBtns = [...document.querySelectorAll(".close")];
+	closeBtns.forEach(function(btn){
+  		btn.onclick = function() {
+    			var modal = btn.closest('.modal');
+    			modal.style.display = "none";
+  		}
+	});
+
+	window.onclick = function(event) {
+  		if (event.target.className === "modal") {
+    			event.target.style.display = "none";
+  		}
+	}
+})(jQuery);
